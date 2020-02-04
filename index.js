1bfee7179d1c9c2f99d053d9606ab5e1ba85eade
Santtu Lempinen <santtu.lempinen@helsinki.fi>
2020-02-04 23:07:26 +0200

    Add better examples

--- index.js
+++ index.js
@@ -4,14 +4,24 @@ console.log(process.env.TOKEN)
 
 const laudebot = new Telegraf(process.env.TOKEN)
 
+// runs on command /destroy
+laudebot.command('destroy', ctx => ctx.reply('commensing destruction of all human life'))
+
+// runs on command /start and is equivalent to laudebot.command('start', ctx => ...)
 laudebot.start(ctx => ctx.reply('At your service'))
 
-laudebot.on('text', ctx => {
-  if (ctx.message.text === 'Hello') {
-    ctx.reply('World')
-  } else {
-    ctx.reply('Happy hacking!')
-  }
+// runs when audio is sent to the bot
+laudebot.on('audio', ctx => ctx.reply('You sound like an angel!'))
+
+// runs when a sticker or a photo is sent to the bot
+laudebot.on(['sticker', 'photo'], ctx => {
+  // use ctx.message to access information about the message
+  const chatId = ctx.message.chat.id
+  // this is equivalent to ctx.reply(...)
+  return ctx.telegram.sendMessage(chatId, 'Have you lost weight?')
 })
+
+// runs when someone types 'wiener' to the bot
+laudebot.hears('wiener', ctx => ctx.reply('snitchel'))
 
 laudebot.launch()
