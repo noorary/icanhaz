3227da022cdd9d9a55e71766eaafc76237c0dca3
noorarytila <rytilanoora@gmail.com>
2020-03-13 18:15:14 +0200

    start reply added

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ require('dotenv').config()
 console.log(process.env.TOKEN)
 
 const icanhazbot = new Telegraf(process.env.TOKEN)
+
+icanhazbot.start(ctx => ctx.reply("Welcome to ICanHazBot!"))
+
+icanhazbot.command('hello', ctx => ctx.reply('Hello'));
+
+icanhazbot.launch()
