3b895d13317b8bb486a2ceb79d23cef105cfd804
noorarytila <rytilanoora@gmail.com>
2020-03-13 18:35:19 +0200

    catlist added

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const icanhazbot = new Telegraf(process.env.TOKEN)
 
 icanhazbot.start(ctx => ctx.reply("Welcome to ICanHazBot!"))
 
+var cats = ["Luke", "Leia", "Mökö", "Manteli"]
+
+icanhazbot.command('showcats', ctx => ctx.reply(cats[0]))
+
 icanhazbot.command('hello', ctx => ctx.reply('Hello'));
 
 icanhazbot.launch()
