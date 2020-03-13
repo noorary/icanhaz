b8d32e2fd11a9ad024ea2fd16c25e321b3214611
em-t <eva.taivalsaari@gmail.com>
2020-03-13 19:03:49 +0200

    list all cats

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-const Telegraf = require('telegraf')
-require('dotenv').config()
-console.log(process.env.TOKEN)
+const Telegraf = require('telegraf');
+require('dotenv').config();
+console.log(process.env.TOKEN);
 
-const icanhazbot = new Telegraf(process.env.TOKEN)
+const icanhazbot = new Telegraf(process.env.TOKEN);
 
-icanhazbot.start(ctx => ctx.reply("Welcome to ICanHazBot!"))
+icanhazbot.start((ctx) => ctx.reply('Welcome to ICanHazBot!'));
 
-var cats = ["Luke", "Leia", "Mökö", "Manteli"]
+var cats = ['Luke', 'Leia', 'Mökö', 'Manteli'];
 
-icanhazbot.command('showcats', ctx => ctx.reply(cats[0]))
+icanhazbot.command('showcats', (ctx) => printCats(ctx));
 
-icanhazbot.command('hello', ctx => ctx.reply('Hello'));
+function printCats(ctx) {
+    if (cats.length == 0) {
+        ctx.reply('no catz :c');
+    } else {
+        var tempStr = '';
+        for (var i = 0; i < cats.length - 1; i++) {
+            tempStr = tempStr.concat(cats[i] + '\n');
+        }
+        tempStr = tempStr.concat(cats[cats.length - 1]);
+        ctx.reply(tempStr);
+    }
+}
 
-icanhazbot.launch()
+icanhazbot.command('hello', (ctx) => ctx.reply('Hello'));
+
+icanhazbot.launch();
