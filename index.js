79e24d1212afa0226e2a9133ec28e0045cf605fd
em-t <eva.taivalsaari@gmail.com>
2020-03-13 19:07:43 +0200

    list tasks

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const icanhazbot = new Telegraf(process.env.TOKEN);
 icanhazbot.start((ctx) => ctx.reply('Welcome to ICanHazBot!'));
 
 var cats = ['Luke', 'Leia', 'Mökö', 'Manteli'];
+var tasks = [];
 
 icanhazbot.command('showcats', (ctx) => printCats(ctx));
+icanhazbot.command('showtasks', (ctx) => printTasks(ctx));
 
 function printCats(ctx) {
     if (cats.length == 0) {
@@ -19,6 +21,19 @@ function printCats(ctx) {
             tempStr = tempStr.concat(cats[i] + '\n');
         }
         tempStr = tempStr.concat(cats[cats.length - 1]);
+        ctx.reply(tempStr);
+    }
+}
+
+function printTasks(ctx) {
+    if (tasks.length == 0) {
+        ctx.reply('can i haz nothing :3');
+    } else {
+        var tempStr = '';
+        for (var i = 0; i < printTasks.length - 1; i++) {
+            tempStr = tempStr.concat(tasks[i] + '\n');
+        }
+        tempStr = tempStr.concat(tasks[tasks.length - 1]);
         ctx.reply(tempStr);
     }
 }
